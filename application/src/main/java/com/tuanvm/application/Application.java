5a9e4a3cbcfe0ce979f466a6484a505cb40c1604
loliconhihi <doanngocduc01102006@gmail.com>
2026-05-24 16:08:00 +0700

    displayed the first fuckin ui

--- application/src/main/java/com/tuanvm/application/Application.java
+++ application/src/main/java/com/tuanvm/application/Application.java
@@ -3,11 +3,9 @@ package com.tuanvm.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.tuanvm")
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ application/src/main/java/com/tuanvm/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.tuanvm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+
+}
