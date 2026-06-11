bc3e1b2b1d01de66f40533c3bda7a5b588d21b42
Thanh <imthegoatonyt@gmail.com>
2026-06-11 13:59:08 +0700

    feat: implement CargoTypePriceManager module with CRUD functionality and dedicated staff layout routes

--- frontend-react/src/features/cargo/routes/CargoRoutes.jsx
+++ frontend-react/src/features/cargo/routes/CargoRoutes.jsx
@@ -1,6 +1,8 @@
 import { Route } from 'react-router-dom';
 import CargoTypeManager from '../components/CargoTypeManager';
+import CargoTypePriceManager from '../components/CargoTypePriceManager';
 
 export const cargoRoutes = [
-    <Route key="cargo-types" path="cargo-types" element={<CargoTypeManager />} />
+    <Route key="cargo-types" path="cargo-types" element={<CargoTypeManager />} />,
+    <Route key="freight-rates" path="freight-rates" element={<CargoTypePriceManager />} />
 ];
