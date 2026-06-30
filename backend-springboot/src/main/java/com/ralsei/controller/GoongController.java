d6ed02a814398b6465833e8dffa473558c0a72aa
Thanh <imthegoatonyt@gmail.com>
2026-06-30 21:11:40 +0700

    add distance and time calculation using Goong

--- backend-springboot/src/main/java/com/ralsei/controller/GoongController.java
+++ backend-springboot/src/main/java/com/ralsei/controller/GoongController.java
@@ -20,4 +20,21 @@ public class GoongController {
         Object response = restTemplate.getForObject(url, Object.class);
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/place/distance-time")
+    public ResponseEntity<Object> getDistanceAndTime(
+            @RequestParam double originLat,
+            @RequestParam double originLng,
+            @RequestParam double destinationLat,
+            @RequestParam double destinationLng,
+            @RequestParam(defaultValue = "car") String vehicle) {
+        String origin = originLat + ", " + originLng;
+        String destination = destinationLat + ", " + destinationLng;
+        String url = "https://rsapi.goong.io/v2/direction?api_key=" + goongApiKey
+                + "&origin=" + origin
+                + "&destination=" + destination
+                + "&vehicle=" + vehicle;
+        Object response = restTemplate.getForObject(url, Object.class);
+        return ResponseEntity.ok(response);
+    }
 }
