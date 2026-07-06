b96eb2fbbb8dc1260fd19a32736cce067dde5bd6
Thanh <imthegoatonyt@gmail.com>
2026-07-07 04:38:41 +0700

    add one to one relationship for ticketAgency and CoachStop

--- backend-springboot/src/main/java/com/ralsei/model/TicketAgency.java
+++ backend-springboot/src/main/java/com/ralsei/model/TicketAgency.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,8 +26,9 @@ public class TicketAgency extends BaseEntity {
     @Column(name = "ticketAgencyId")
     private int ticketAgencyId;
 
-    @Column(name = "stopPointId", nullable = false)
-    private int stopPointId;
+    @OneToOne(fetch = jakarta.persistence.FetchType.LAZY)
+    @jakarta.persistence.JoinColumn(name = "stopPointId", referencedColumnName = "stopPointId", nullable = false)
+    private CoachStop coachStop;
 
     @Column(name = "ticketAgencyName", nullable = false)
     private String ticketAgencyName;
@@ -34,5 +36,4 @@ public class TicketAgency extends BaseEntity {
     @Column(name = "isActive", nullable = false)
     private boolean isActive;
 
-    
 }
