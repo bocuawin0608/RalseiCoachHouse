927f8e53605e54e0b94d3485d0aaf7fde43c168a
Yoshi <hameo.kapy@gmail.com>
2026-07-12 10:02:49 +0700

    update checkin policy interval for trip staff checkin

--- backend-springboot/src/main/java/com/ralsei/repository/CargoTicketRepository.java
+++ backend-springboot/src/main/java/com/ralsei/repository/CargoTicketRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ralsei.model.CargoTicket;
 
@@ -11,4 +13,7 @@ public interface CargoTicketRepository extends JpaRepository<CargoTicket, Intege
     Optional<CargoTicket> findByTicketCode(String ticketCode);
 
     List<CargoTicket> findByTripId(int tripId);
+
+    @Query("SELECT ct FROM CargoTicket ct WHERE ct.tripId = :tripId AND ct.status IN :statuses ORDER BY ct.cargoTicketId ASC")
+    List<CargoTicket> findByTripIdAndStatusIn(@Param("tripId") int tripId, @Param("statuses") List<String> statuses);
 }
